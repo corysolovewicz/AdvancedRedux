a1dc76e903838af8ee408041cbd8d44c3e8da1ac
CorySolovewicz <CorySolovewicz>
2016-12-07 17:01:05 -0800

    Adding Signin Form

--- chapter7/src/components/app.js
+++ chapter7/src/components/app.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import Header from './header';
+import React from 'react';
+import { Component } from 'react';
 
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      <Header />
-      Redux simple starter
+        <Header />
+        {this.props.children}
       </div>
     );
   }
