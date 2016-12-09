1dea3250fd50ef61ea06582b59e54ef69219a595
CorySolovewicz <CorySolovewicz>
2016-12-08 22:28:52 -0800

    Fix issue where email and password were coming through as undefined. Turned out that the current version of Redux-form (v6) uses Field. The only way to fix it without using field is to rollback redux-form to version 5.3.3 in the package.json

--- chapter7/src/components/auth/signin.js
+++ chapter7/src/components/auth/signin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
   handleFormSubmit({ email, password }) {
@@ -14,11 +14,11 @@ class Signin extends Component {
       <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
           <label>Email:</label>
-          <input {...email} className="form-control" />
+          <Field name="email" component="input" className="form-control" />
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input {...password} className="form-control" />
+          <Field name="password" component="input" className="form-control" />
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign in</button>
       </form>
